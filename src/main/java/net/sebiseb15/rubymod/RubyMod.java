9d11de8a7555ac2886602ccdf491030c1b345f47
Seb <sebastiengibiot@mgail.com>
2025-10-26 11:18:30 +0100

    ajout d'un item group

--- src/main/java/net/sebiseb15/rubymod/RubyMod.java
+++ src/main/java/net/sebiseb15/rubymod/RubyMod.java
@@ -3,12 +3,13 @@ package net.sebiseb15.rubymod;
 import net.fabricmc.api.ModInitializer;
 
 import net.sebiseb15.rubymod.block.ModedBlocks;
+import net.sebiseb15.rubymod.item.ModItemsGroups;
 import net.sebiseb15.rubymod.item.ModedItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RubyMod implements ModInitializer {
-	public static final String MOD_ID = "testmod";
+	public static final String MOD_ID = "rubymod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -16,5 +17,6 @@ public class RubyMod implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
         ModedItems.registerModItems();
         ModedBlocks.registerModBlocks();
+        ModItemsGroups.registerItemGroups();
 	}
 }
---
+++ src/main/java/net/sebiseb15/rubymod/item/ModItemsGroups.java
@@ -0,0 +1,30 @@
+package net.sebiseb15.rubymod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.sebiseb15.rubymod.RubyMod;
+import net.minecraft.registry.Registry;
+import net.sebiseb15.rubymod.block.ModedBlocks;
+
+public class ModItemsGroups {
+
+    public static final ItemGroup Ruby_Items_Group = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(RubyMod.MOD_ID, "ruby_items"),
+            FabricItemGroup.builder().icon(()-> new ItemStack(ModedItems.Ruby))
+                    .displayName(Text.translatable("itemgroup.rubymod.ruby_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(new ItemStack(ModedItems.Ruby));
+                        entries.add(new ItemStack(ModedBlocks.Ruby_Block));
+
+                    }
+            ).build()
+    );
+
+    public static void registerItemGroups() {
+        RubyMod.LOGGER.info("Registering Item Groups for "+ RubyMod.MOD_ID);
+    }
+}
