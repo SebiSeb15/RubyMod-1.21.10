dcc2180a4c6f954f4c6bf60805a4df4964d623de
Seb <sebastiengibiot@mgail.com>
2025-11-09 21:27:04 +0100

    correction de la transparence du cristal

--- src/main/java/net/sebiseb15/rubymod/block/custom/RubyCrystal.java
+++ src/main/java/net/sebiseb15/rubymod/block/custom/RubyCrystal.java
@@ -1,6 +1,7 @@
 package net.sebiseb15.rubymod.block.custom;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
@@ -8,13 +9,34 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.sebiseb15.rubymod.block.ModedBlocks;
 
 public class RubyCrystal extends Block {
     public RubyCrystal(Settings settings) {
-        super(settings);
+        super(settings.nonOpaque()
+                .allowsSpawning((state, world, pos, type) -> false)
+                .solidBlock((state, world, pos) -> false)
+                .suffocates((state, world, pos) -> false)
+                .blockVision((state, world, pos) -> false));
     }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public boolean isSideInvisible(BlockState state, BlockState stateFrom, Direction direction) {
+        // Si le bloc adjacent est du même type, la face n'est pas rendue
+        if (stateFrom.isOf(this)) {
+            return true;
+        }
+        return super.isSideInvisible(state, stateFrom, direction);
+    }
+
+
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
